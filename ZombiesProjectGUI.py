786c2ca6a963f7e895737d34d715496b2cb60ebb
Samjohn03 <sj.tilley23@gmail.com>
2022-11-04 17:17:37 -0400

    All lists condensed into 1 .py file and imported

--- ZombiesProjectGUI.py
+++ ZombiesProjectGUI.py
@@ -4,33 +4,24 @@ import random
 import math
 import PygameTWF
 import PygameCTBF
-import VariousLists
+from VariousLists import Challenge, bDescriptions, redGreen, Map, rangesList, extraInfoDesc, cDescriptions
 pygame.init()
 check = False
 window = pygame.display.set_mode([800,800])
-Type = pygame.font.SysFont("impact", 80)        
-Type2 = pygame.font.SysFont("Playfair Display",50)
-Type3 = pygame.font.SysFont("Tahoma",15)
+fontFace = pygame.font.SysFont("impact", 80)        
+fontFace2 = pygame.font.SysFont("Playfair Display",50)
+fontFace3 = pygame.font.SysFont("Tahoma",15)
 previousClick = False
 cyanTimer = 0
-run = True
+gameGoBRRR = True
 secondScreen = False
 
-extraInfoDesc3 = open("ExtraInfoDescriptions.txt", "r")
-extraInfoDesc2 = extraInfoDesc3.read()
-extraInfoDesc = extraInfoDesc2.split("3g")
-extraInfoDesc3.close()
-cDescriptions3 = open("cDescriptions.txt", "r")
-cDescriptions2 = cDescriptions3.read()
-cDescriptions = cDescriptions2.split("3g")
-cDescriptions3.close()
-
 clock = pygame.time.Clock()
-while run:
+while gameGoBRRR:
     window.fill((119, 119, 119))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            gameGoBRRR = False
     window.fill((140,140,140))
     Click = pygame.mouse.get_pressed()
 
@@ -38,9 +29,9 @@ while run:
     if secondScreen == False:
         pygame.draw.rect(window, (230,20,20),(145,145,500,500))
         pygame.draw.rect(window, (0,0,0),(145,145,500,500),5)
-        generateC = Type.render("Generate", True, (0,0,0))
+        generateC = fontFace.render("Generate", True, (0,0,0))
         window.blit(generateC, (245,300))
-        gChallenge = Type.render("Challenge", True, (0,0,0))
+        gChallenge = fontFace.render("Challenge", True, (0,0,0))
         window.blit(gChallenge, (231,400))
 
     # Determining if the user is on the second screen or not
@@ -64,7 +55,7 @@ while run:
         else:
             pygame.draw.rect(window, (80,80,80),(690,10,100,50))
         pygame.draw.rect(window, (30,30,30), (690,10,100,50),5)
-        redo = Type2.render("redo",True, (0,0,0))
+        redo = fontFace2.render("redo",True, (0,0,0))
         window.blit(redo, (703,18))
         
         #Challenge Rule Boxes
@@ -73,7 +64,7 @@ while run:
             pygame.draw.rect(window, (30,30,30), (10,50 + rectdraw2 * 50,35,35),5)
             rectdraw2 = rectdraw2 - 1
         for index, amount in enumerate(bDescriptions):
-            bDRender = Type2.render(amount,True,(0,0,0))
+            bDRender = fontFace2.render(amount,True,(0,0,0))
             window.blit(bDRender, (50,100 + index * 50))
         pygame.draw.line(window, (0,0,0),(0,90),(360,90),5)
         pygame.draw.line(window, (0,0,0),(0,494),(360,494),5)
@@ -99,7 +90,7 @@ while run:
             check = True
 
         # Challenge Descriptions
-        PygameTWF.renderTextWrap((cDescriptions[challengeNumber]), Type2, (0,0,0), 795, window, 0,503,50)
+        PygameTWF.renderTextWrap((cDescriptions[challengeNumber]), fontFace2, (0,0,0), 795, window, 0,503,50)
 
         #Red and Green Boxes 
         redgreen2 = (redGreen[challengeNumber]).split()
@@ -107,13 +98,13 @@ while run:
             pygame.draw.rect(window, (amount), (15,105 + index * 50,25,25))
 
         # Displaying the Map and Challenge
-        mapName = Type2.render(str(Map[mapNumber]), True, (0,0,0))
-        challengeName = Type2.render(str(Challenge[challengeNumber]),True, (0,0,0))
+        mapName = fontFace2.render(str(Map[mapNumber]), True, (0,0,0))
+        challengeName = fontFace2.render(str(Challenge[challengeNumber]),True, (0,0,0))
         window.blit(mapName, (10,10))
         window.blit(challengeName, (10,50))
 
         #Fps Counter
-        fPS = Type3.render(str(math.trunc(clock.get_fps())) + " fps", True, (0,0,0))
+        fPS = fontFace3.render(str(math.trunc(clock.get_fps())) + " fps", True, (0,0,0))
         window.blit(fPS, (755,780))
     
         # Extra info on hover
@@ -122,7 +113,7 @@ while run:
                 rangesList2 = amount.split("3g")
                 if int(rangesList2[0]) <= pygame.mouse.get_pos()[1] < int(rangesList2[1]):
                     displayExtraInfo = index
-            PygameCTBF.cursorTextBox((extraInfoDesc[displayExtraInfo]),Type3,(0,0,0),200,window,15,(190,190,190))
+            PygameCTBF.cursorTextBox((extraInfoDesc[displayExtraInfo]),fontFace3,(0,0,0),200,window,15,(190,190,190))
 
     previousClick = Click[0]
     pygame.display.flip()
